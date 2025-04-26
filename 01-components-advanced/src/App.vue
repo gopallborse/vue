e36c783e0f463fc6a609262bce347ce4642b033a
Gopall Borse <gopallborse@gmail.com>
2025-04-26 17:36:42 +0530

    slots, named slots, scoped slots

--- 01-components-advanced/src/App.vue
+++ 01-components-advanced/src/App.vue
@@ -8,6 +8,12 @@
       :info-text="activeUser.description"
       :role="activeUser.role"
     ></user-info>
+    <CourseGoals>
+      <template #default="slotProps">
+        <h2>{{ slotProps.item }}</h2>
+        <p>{{ slotProps['another-prop'] }}</p>
+      </template>
+    </CourseGoals>
   </div>
 </template>
 
@@ -15,6 +21,7 @@
 import TheHeader from "./components/TheHeader.vue";
 import BadgeList from "./components/BadgeList.vue";
 import UserInfo from "./components/UserInfo.vue";
+import CourseGoals from "./components/CourseGoals.vue";
 
 export default {
   components: {
@@ -23,6 +30,7 @@ export default {
     TheHeader,
     BadgeList,
     UserInfo,
+    CourseGoals,
   },
   data() {
     return {
---
+++ 01-components-advanced/src/components/BaseCard.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <header>
+      <slot name="header" v-if="$slots.header">
+        <!-- <h1>The Default!</h1> -->
+        <!-- Default content if not provided where slot content not provided. -->
+      </slot>
+    </header>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    console.log(this.$slots.header);
+  },
+};
+</script>
+
+<style scoped>
+div {
+  margin: 2rem auto;
+  max-width: 30rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 1rem;
+}
+
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
